513cdaef0c8fd13239fa97d80689b4788ead8b4a
Deepanshu Jindal <deepanshujindal.99@gmail.com>
2018-07-05 19:09:47 +0800

    fixed typo

--- src/DissectTrafficPcap.py
+++ src/DissectTrafficPcap.py
@@ -212,115 +212,115 @@ def host_network_conversion(field):
 
 # makes the actual lua script given the relevant header type and next and previous state transition information
 def make_template(control_graph, header, header_type, destination, header_ports):
-    if ((ETHER_DETECT or IPv4_DETECT or IPv6_DETECT or TCP_DETECT or UDP_DETECT) == False):
-        headerUpper = header.upper()
-        fout_header = open(destination + ".h","w")
-        fout_source = open(destination + ".cpp","w")
+    
+    headerUpper = header.upper()
+    fout_header = open(destination + ".h","w")
+    fout_source = open(destination + ".cpp","w")
 
-        fout_header.write("//Template for addition of new protocol '%s'\n\n" %(header))
-        fout_header.write("#ifndef %s\n" %("P4_"+header.upper()+"_LAYER"))
-        fout_header.write("#define %s\n\n" %("P4_"+header.upper()+"_LAYER"))
-        fout_header.write("#include \"Layer.h\"\n")
-        fout_header.write("#ifdef defined(WIN32) || defined(WINx64)\n#include <winsock2.h>\n#elif LINUX\n#include <in.h>\n#endif\n\n")
-        fout_header.write("namespace pcpp{\n\t#pragma pack(push,1)\n")
-        fout_header.write("\tstruct %s{\n" %(header.lower()+"hdr"))
-        
-        variable_fields = []
+    fout_header.write("//Template for addition of new protocol '%s'\n\n" %(header))
+    fout_header.write("#ifndef %s\n" %("P4_"+header.upper()+"_LAYER"))
+    fout_header.write("#define %s\n\n" %("P4_"+header.upper()+"_LAYER"))
+    fout_header.write("#include \"Layer.h\"\n")
+    fout_header.write("#ifdef defined(WIN32) || defined(WINx64)\n#include <winsock2.h>\n#elif LINUX\n#include <in.h>\n#endif\n\n")
+    fout_header.write("namespace pcpp{\n\t#pragma pack(push,1)\n")
+    fout_header.write("\tstruct %s{\n" %(header.lower()+"hdr"))
+    
+    variable_fields = []
+    for field in header_type["fields"]:
+        try:
+            fout_header.write("\t\t%s \t %s;\n" %(predict_type(field[1]),field[0]))
+        except TypeError:
+            variable_fields.append(field[0])
+    if (len(variable_fields)>0):
+        fout_header.write("\n\t\t// variable length fields\n")
+    for variable_field in variable_fields:
+        fout_header.write("%s\n" %(variable_field))
+    fout_header.write("\t};\n\n")
+
+    fout_header.write("\t#pragma pack(pop)\n")
+    fout_header.write("\tclass %sLayer: public Layer{\n" %(header.capitalize()))
+    fout_header.write("\t\tpublic:\n")
+    fout_header.write("\t\t %sLayer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet): Layer(data, dataLen, prevLayer, packet) {m_Protocol = P4_%s;}\n" %(header.capitalize(), header.upper()))
+   
+    fout_header.write("\n\t\t // Getters for fields\n")
+
+    for field in header_type["fields"]:
+        try:
+            fout_header.write("\t\t %s get%s();\n" %(predict_type(field[1]),str(field[0]).capitalize()))
+        except TypeError:
+            field[1] = int(input('Variable length field ' + field[0] + ' detected in ' + header + '. Enter its length\n'))
+            fout_header.write("\t\t %s get%s();\n" %(predict_type(field[1]),str(field[0]).capitalize()))
+
+    fout_header.write("\n\t\t inline %shdr* get%sHeader() { return (%shdr*)m_Data; }\n\n" %(header.lower(), header.capitalize(), header.lower()))
+    fout_header.write("\t\t void parseNextLayer();\n\n")
+    fout_header.write("\t\t inline size_t getHeaderLen() { return sizeof(%shdr); }\n\n" %(header.lower()))
+    fout_header.write("\t\t void computeCalculateField() {}\n\n")
+    fout_header.write("\t\t std::string toString();\n\n")
+    fout_header.write("\t\t OsiModelLayer getOsiModelLayer() { return OsiModelApplicationLayer; }\n\n")
+    fout_header.write("\t};\n")
+    fout_header.write("}\n#endif")
+    fout_header.close()
+
+    fout_source.write("#define LOG_MODULE PacketLogModule%sLayer\n\n" %(header.capitalize()))
+    fout_source.write("#include \"%sLayer.h\"\n" %(header.capitalize()))
+    fout_source.write("#include \"PayloadLayer.h\"\n#include \"IpUtils.h\"\n#include \"Logger.h\"\n")
+    fout_source.write("#include <string.h>\n#include <sstream>\n#include <endian.h>\n\n")
+    fout_source.write("namespace pcpp{\n")
+
+    for field in header_type["fields"]:
+        try:
+            fout_source.write("\t%s %sLayer::get%s(){\n" %(predict_type(field[1]), header.capitalize(), str(field[0]).capitalize()))
+            fout_source.write("\t\t%s %s;\n" %(predict_type(field[1]), field[0]))
+            fout_source.write("\t\t%shdr* hdrdata = (%shdr*)m_Data;\n" %(header.lower(),header.lower()))
+            fout_source.write("\t\t%s = %s(hdrdata->%s);\n" %(field[0],host_network_conversion(field), field[0]))
+            fout_source.write("\t\treturn %s;\n\t}\n\n" %(field[0]))
+        except TypeError:
+            field[1] = int(input('Variable length field ' + field[0] + ' detected in ' + header + '. Enter its length\n'))
+            fout_source.write("\t%s %sLayer::get%s(){\n" %(predict_type(field[1]), header.capitalize(), str(field[0]).capitalize()))
+            fout_source.write("\t\t%s %s;\n" %(predict_type(field[1]), field[0]))
+            fout_source.write("\t\t%shdr* hdrdata = (%shdr*)m_Data;\n" %(header.lower(),header.lower()))
+            fout_source.write("\t\t%s = %s(hdrdata->%s);\n" %(field[0],host_network_conversion(field), field[0]))
+            fout_source.write("\t\treturn %s;\n\t}\n\n" %(field[0]))
+
+    default_next_transition = None
+    transition_key = None
+    next_transitions = []
+    for edge in control_graph:
+        if (header==edge[0]):
+            if (edge[1]!=None):
+                transition_key = edge[1]
+                next_transitions.append((edge[-1],edge[-2]))
+            else:
+                default_next_transition = edge[-1]
+
+    fout_source.write("\tvoid %sLayer::parseNextLayer(){\n" %(header.capitalize()))
+    fout_source.write("\t\tif (m_DataLen <= sizeof(%shdr))\n" %(header.lower()))
+    fout_source.write("\t\t\treturn;\n\n")
+
+    if (len(next_transitions)>0):
+        fout_source.write("\t\t%shdr* hdrdata = get%sHeader();\n" %(header.lower(), header.capitalize()))
         for field in header_type["fields"]:
-            try:
-                fout_header.write("\t\t%s \t %s;\n" %(predict_type(field[1]),field[0]))
-            except TypeError:
-                variable_fields.append(field[0])
-        if (len(variable_fields)>0):
-            fout_header.write("\n\t\t// variable length fields\n")
-        for variable_field in variable_fields:
-            fout_header.write("%s\n" %(variable_field))
-        fout_header.write("\t};\n\n")
-
-        fout_header.write("\t#pragma pack(pop)\n")
-        fout_header.write("\tclass %sLayer: public Layer{\n" %(header.capitalize()))
-        fout_header.write("\t\tpublic:\n")
-        fout_header.write("\t\t %sLayer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet): Layer(data, dataLen, prevLayer, packet) {m_Protocol = P4_%s;}\n" %(header.capitalize(), header.upper()))
-       
-        fout_header.write("\n\t\t // Getters for fields\n")
-
-        for field in header_type["fields"]:
-            try:
-                fout_header.write("\t\t %s get%s();\n" %(predict_type(field[1]),str(field[0]).capitalize()))
-            except TypeError:
-                field[1] = int(input('Variable length field ' + field[0] + ' detected in ' + header + '. Enter its length\n'))
-                fout_header.write("\t\t %s get%s();\n" %(predict_type(field[1]),str(field[0]).capitalize()))
-
-        fout_header.write("\n\t\t inline %shdr* get%sHeader() { return (%shdr*)m_Data; }\n\n" %(header.lower(), header.capitalize(), header.lower()))
-        fout_header.write("\t\t void parseNextLayer();\n\n")
-        fout_header.write("\t\t inline size_t getHeaderLen() { return sizeof(%shdr); }\n\n" %(header.lower()))
-        fout_header.write("\t\t void computeCalculateField() {}\n\n")
-        fout_header.write("\t\t std::string toString();\n\n")
-        fout_header.write("\t\t OsiModelLayer getOsiModelLayer() { return OsiModelApplicationLayer; }\n\n")
-        fout_header.write("\t};\n")
-        fout_header.write("}\n#endif")
-        fout_header.close()
-
-        fout_source.write("#define LOG_MODULE PacketLogModule%sLayer\n\n" %(header.capitalize()))
-        fout_source.write("#include \"%sLayer.h\"\n" %(header.capitalize()))
-        fout_source.write("#include \"PayloadLayer.h\"\n#include \"IpUtils.h\"\n#include \"Logger.h\"\n")
-        fout_source.write("#include <string.h>\n#include <sstream>\n#include <endian.h>\n\n")
-        fout_source.write("namespace pcpp{\n")
-
-        for field in header_type["fields"]:
-            try:
-                fout_source.write("\t%s %sLayer::get%s(){\n" %(predict_type(field[1]), header.capitalize(), str(field[0]).capitalize()))
-                fout_source.write("\t\t%s %s;\n" %(predict_type(field[1]), field[0]))
-                fout_source.write("\t\t%shdr* hdrdata = (%shdr*)m_Data;\n" %(header.lower(),header.lower()))
-                fout_source.write("\t\t%s = %s(hdrdata->%s);\n" %(field[0],host_network_conversion(field), field[0]))
-                fout_source.write("\t\treturn %s;\n\t}\n\n" %(field[0]))
-            except TypeError:
-                field[1] = int(input('Variable length field ' + field[0] + ' detected in ' + header + '. Enter its length\n'))
-                fout_source.write("\t%s %sLayer::get%s(){\n" %(predict_type(field[1]), header.capitalize(), str(field[0]).capitalize()))
-                fout_source.write("\t\t%s %s;\n" %(predict_type(field[1]), field[0]))
-                fout_source.write("\t\t%shdr* hdrdata = (%shdr*)m_Data;\n" %(header.lower(),header.lower()))
-                fout_source.write("\t\t%s = %s(hdrdata->%s);\n" %(field[0],host_network_conversion(field), field[0]))
-                fout_source.write("\t\treturn %s;\n\t}\n\n" %(field[0]))
-
-        default_next_transition = None
-        transition_key = None
-        next_transitions = []
-        for edge in control_graph:
-            if (header==edge[0]):
-                if (edge[1]!=None):
-                    transition_key = edge[1]
-                    next_transitions.append((edge[-1],edge[-2]))
-                else:
-                    default_next_transition = edge[-1]
-
-        fout_source.write("\tvoid %sLayer::parseNextLayer(){\n" %(header.capitalize()))
-        fout_source.write("\t\tif (m_DataLen <= sizeof(%shdr))\n" %(header.lower()))
-        fout_source.write("\t\t\treturn;\n\n")
-
-        if (len(next_transitions)>0):
-            fout_source.write("\t\t%shdr* hdrdata = get%sHeader();\n" %(header.lower(), header.capitalize()))
-            for field in header_type["fields"]:
-                if (field[0]==transition_key):
-                    size = field[1]
-                    break
-            fout_source.write("\t\t%s %s = %s(hdrdata->%s);\n\t\t" %(predict_type(field[1]), transition_key,host_network_conversion(field), transition_key))
-            for transition in next_transitions[:-1]:
-                #print transition
-                fout_source.write("if (%s == %s)\n" %(transition_key, transition[1]))
-                fout_source.write("\t\t\tm_NextLayer = new %sLayer(m_Data+sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n" %(transition[0].capitalize(),header.lower(), header.lower()))
-                fout_source.write("\t\telse ")
-            transition = next_transitions[-1]
+            if (field[0]==transition_key):
+                size = field[1]
+                break
+        fout_source.write("\t\t%s %s = %s(hdrdata->%s);\n\t\t" %(predict_type(field[1]), transition_key,host_network_conversion(field), transition_key))
+        for transition in next_transitions[:-1]:
+            #print transition
             fout_source.write("if (%s == %s)\n" %(transition_key, transition[1]))
             fout_source.write("\t\t\tm_NextLayer = new %sLayer(m_Data+sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n" %(transition[0].capitalize(),header.lower(), header.lower()))
+            fout_source.write("\t\telse ")
+        transition = next_transitions[-1]
+        fout_source.write("if (%s == %s)\n" %(transition_key, transition[1]))
+        fout_source.write("\t\t\tm_NextLayer = new %sLayer(m_Data+sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n" %(transition[0].capitalize(),header.lower(), header.lower()))
 
-            if (default_next_transition!=None):
-                fout_source.write("\t\telse\n")
-                if (default_next_transition=="final"):
-                    fout_source.write("\t\t\tm_NextLayer = new PayloadLayer(m_Data + sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n\t}\n" %(header.lower(),header.lower()))
-                else:
-                    fout_source.write("\t\t\tm_NextLayer = new default_next_transition(m_Data + sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n\t}\n" %(header.lower(),header.lower()))
+        if (default_next_transition!=None):
+            fout_source.write("\t\telse\n")
+            if (default_next_transition=="final"):
+                fout_source.write("\t\t\tm_NextLayer = new PayloadLayer(m_Data + sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n\t}\n" %(header.lower(),header.lower()))
+            else:
+                fout_source.write("\t\t\tm_NextLayer = new default_next_transition(m_Data + sizeof(%shdr), m_DataLen - sizeof(%shdr), this, m_Packet);\n\t}\n" %(header.lower(),header.lower()))
 
-        fout_source.write("\n\tstd::string %sLayer::toString(){}\n\n" %(header.capitalize()))
+    fout_source.write("\n\tstd::string %sLayer::toString(){}\n\n" %(header.capitalize()))
 
 
 control_graph = make_control_graph(data["parsers"])
@@ -336,8 +336,9 @@ local_name = local_name[local_name.rfind('/')+1:local_name.rfind('.')]
 for i in range(len(header_ports)):
     if ((ETHER_DETECT and header_ports[i]=='ethernet') or (IPv4_DETECT and header_ports[i]=='ipv4') or (IPv6_DETECT and header_ports[i]=='ipv6') or (TCP_DETECT and header_ports[i]=='tcp') or (UDP_DETECT and header_ports[i]=='udp')):
         continue
+    
     destination = DESTINATION + local_name + "_" + \
-        header_ports[i]
+    header_ports[i]
     make_template(control_graph, header_ports[i], header_types[i], destination, header_ports)
 
 if (DEBUG):
