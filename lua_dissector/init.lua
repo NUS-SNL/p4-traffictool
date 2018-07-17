4c4f8f71a03504972fa0f4ccd8eb547cdeb8026e
deepanshu jindal <deepanshujindal.99@gmail.com>
2018-07-17 17:12:28 +0800

    removed redirection of compiler output

--- lua_dissector/init.lua
+++
@@ -1,4 +0,0 @@
-dofile('/home/deepanshu/p4-traffictools/lua_dissector/arp_1_arp.lua')
-dofile('/home/deepanshu/p4-traffictools/lua_dissector/arp_2_arp_ipv4.lua')
-dofile('/home/deepanshu/p4-traffictools/lua_dissector/arp_3_ipv4.lua')
-dofile('/home/deepanshu/p4-traffictools/lua_dissector/arp_4_icmp.lua')
