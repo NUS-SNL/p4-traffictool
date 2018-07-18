6e96c96f4fb6872b1b2b3987afaa8a01a0c03a64
Deepanshu Jindal <deepanshujindal.99@gmail.com>
2018-07-18 19:15:11 +0800

    fixed naming convention for ipv4 forward

--- runsamples.sh
+++ runsamples.sh
@@ -3,7 +3,7 @@ mkdir -p samples/arp/output
 mkdir -p samples/basic_tunnel/output
 mkdir -p samples/dummy-for-dissector/output
 mkdir -p samples/heavy_hitter/output
-mkdir -p samples/ipv4/output
+mkdir -p samples/ipv4_forward/output
 mkdir -p samples/mri/output
 mkdir -p samples/simple_nat/output
 mkdir -p samples/simple_router/output
@@ -15,7 +15,7 @@ yes n|./p4-traffictools.sh -p4 samples/arp/arp.p4 --std p4-16 -o samples/arp/out
 yes n|./p4-traffictools.sh -p4 samples/basic_tunnel/basic_tunnel.p4 --std p4-16 -o samples/basic_tunnel/output --scapy --wireshark --moongen --pcpp --debug
 yes n|./p4-traffictools.sh -p4 samples/dummy-for-dissector/dissector_dummy.p4 --std p4-14 -o samples/dummy-for-dissector/output --scapy --wireshark --moongen --pcpp --debug
 yes n|./p4-traffictools.sh -p4 samples/heavy_hitter/heavy_hitter.p4 --std p4-14 -o samples/heavy_hitter/output --scapy --wireshark --moongen --pcpp --debug
-yes n|./p4-traffictools.sh -p4 samples/ipv4/ipv4_forward.p4 --std p4-16 -o samples/ipv4/output --scapy --wireshark --moongen --pcpp --debug
+yes n|./p4-traffictools.sh -p4 samples/ipv4/ipv4_forward.p4 --std p4-16 -o samples/ipv4_forward/output --scapy --wireshark --moongen --pcpp --debug
 yes n|./p4-traffictools.sh -p4 samples/mri/mri.p4 --std p4-16 -o samples/mri/output --scapy --wireshark --moongen --pcpp --debug
 yes n|./p4-traffictools.sh -p4 samples/simple_nat/simple_nat.p4 --std p4-14 -o samples/simple_nat/output --scapy --wireshark --moongen --pcpp --debug
 yes n|./p4-traffictools.sh -p4 samples/simple_router/simple_router.p4 --std p4-14 -o samples/simple_router/output --scapy --wireshark --moongen --pcpp --debug
@@ -30,7 +30,7 @@ yes n|./p4-traffictools.sh -p4 samples/arp/arp.p4 --std p4-16 -o samples/arp/out
 yes n|./p4-traffictools.sh -p4 samples/basic_tunnel/basic_tunnel.p4 --std p4-16 -o samples/basic_tunnel/output --scapy --wireshark --moongen --pcpp 
 yes n|./p4-traffictools.sh -p4 samples/dummy-for-dissector/dissector_dummy.p4 --std p4-14 -o samples/dummy-for-dissector/output --scapy --wireshark --moongen --pcpp 
 yes n|./p4-traffictools.sh -p4 samples/heavy_hitter/heavy_hitter.p4 --std p4-14 -o samples/heavy_hitter/output --scapy --wireshark --moongen --pcpp 
-yes n|./p4-traffictools.sh -p4 samples/ipv4/ipv4_forward.p4 --std p4-16 -o samples/ipv4/output --scapy --wireshark --moongen --pcpp 
+yes n|./p4-traffictools.sh -p4 samples/ipv4/ipv4_forward.p4 --std p4-16 -o samples/ipv4_forward/output --scapy --wireshark --moongen --pcpp 
 yes n|./p4-traffictools.sh -p4 samples/mri/mri.p4 --std p4-16 -o samples/mri/output --scapy --wireshark --moongen --pcpp 
 yes n|./p4-traffictools.sh -p4 samples/simple_nat/simple_nat.p4 --std p4-14 -o samples/simple_nat/output --scapy --wireshark --moongen --pcpp 
 yes n|./p4-traffictools.sh -p4 samples/simple_router/simple_router.p4 --std p4-14 -o samples/simple_router/output --scapy --wireshark --moongen --pcpp 
