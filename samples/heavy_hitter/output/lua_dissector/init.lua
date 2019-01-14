4925208e5677035802126270e71f0101dfef909f
Raj Joshi <rajkiranjoshi@gmail.com>
2019-01-14 20:27:59 +0530

    Samples clean-up

--- samples/heavy_hitter/output/lua_dissector/init.lua
+++
@@ -1,2 +0,0 @@
-dofile('/home/deepanshu/p4-traffictool/samples/heavy_hitter/output/lua_dissector/heavy_hitter_1_ipv4.lua')
-dofile('/home/deepanshu/p4-traffictool/samples/heavy_hitter/output/lua_dissector/heavy_hitter_2_tcp.lua')
