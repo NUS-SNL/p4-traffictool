ec1141e28df4bc5b2bf74435179b143993c152df
Raj Joshi <rajkiranjoshi@gmail.com>
2020-12-23 20:20:27 +0800

    Moved runsamples.sh

---
+++ tests/runsamples.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm -rf ../samples/basic_postcards/output
+rm -rf ../samples/basic_tunnel/output
+rm -rf ../samples/hula/output
+rm -rf ../samples/linear_road/output
+rm -rf ../samples/mri/output
+rm -rf ../samples/netcache/output
+rm -rf ../samples/netchain/output
+rm -rf ../samples/p4paxos/output
+rm -rf ../samples/qmetadata/output
+rm -rf ../samples/src_routing/output
+
+# for running in debug mode
+yes y|../p4-traffictool -json ../samples/basic_postcards/basic_postcards.json --std p4-16 -o ../samples/basic_postcards/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/basic_tunnel/basic_tunnel.json --std p4-16 -o ../samples/basic_tunnel/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/hula/hula.json --std p4-16 -o ../samples/hula/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/linear_road/linear_road.json --std p4-14 -o ../samples/linear_road/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/mri/mri.json --std p4-16 -o ../samples/mri/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/netchain/netchain.json --std p4-16 -o ../samples/netchain/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/netcache/netcache.json --std p4-16 -o ../samples/netcache/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/p4paxos/paxos_acceptor.json --std p4-14 -o ../samples/p4paxos/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/src_routing/src_routing.json --std p4-16 -o ../samples/src_routing/output --scapy --wireshark --moongen --pcpp --debug
+yes y|../p4-traffictool -json ../samples/qmetadata/qmetadata.json --std p4-16 -o ../samples/qmetadata/output --scapy --wireshark --moongen --pcpp --debug
